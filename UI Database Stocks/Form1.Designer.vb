b7d64fd71ffe29380c9590635c512ea148b64ecc
JustynOwen <justynwen0@gmail.com>
2025-06-06 14:39:43 +0700

    Design Products

--- UI Database Stocks/Form1.Designer.vb
+++ UI Database Stocks/Form1.Designer.vb
@@ -22,52 +22,135 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnINSERT = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnUPDATE = New System.Windows.Forms.Button()
+        Me.btnDELETE = New System.Windows.Forms.Button()
+        Me.btnCANCEL = New System.Windows.Forms.Button()
+        Me.txtNAMABARANG = New System.Windows.Forms.TextBox()
+        Me.txtHARGA = New System.Windows.Forms.TextBox()
+        Me.txtMEREKBARANG = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'Label1
+        'btnINSERT
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(55, 72)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.btnINSERT.Location = New System.Drawing.Point(43, 140)
+        Me.btnINSERT.Name = "btnINSERT"
+        Me.btnINSERT.Size = New System.Drawing.Size(75, 23)
+        Me.btnINSERT.TabIndex = 1
+        Me.btnINSERT.Text = "&INSERT"
+        Me.btnINSERT.UseVisualStyleBackColor = True
         '
-        'Button1
+        'Label2
         '
-        Me.Button1.Location = New System.Drawing.Point(58, 162)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(123, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "NamaBarang :"
         '
-        'TextBox1
+        'Label3
         '
-        Me.TextBox1.Location = New System.Drawing.Point(174, 69)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 2
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 102)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(126, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "MerekBarang :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 67)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Harga :"
+        '
+        'btnUPDATE
+        '
+        Me.btnUPDATE.Location = New System.Drawing.Point(124, 140)
+        Me.btnUPDATE.Name = "btnUPDATE"
+        Me.btnUPDATE.Size = New System.Drawing.Size(75, 23)
+        Me.btnUPDATE.TabIndex = 9
+        Me.btnUPDATE.Text = "&UPDATE"
+        Me.btnUPDATE.UseVisualStyleBackColor = True
+        '
+        'btnDELETE
+        '
+        Me.btnDELETE.Location = New System.Drawing.Point(205, 140)
+        Me.btnDELETE.Name = "btnDELETE"
+        Me.btnDELETE.Size = New System.Drawing.Size(75, 23)
+        Me.btnDELETE.TabIndex = 10
+        Me.btnDELETE.Text = "&DELETE"
+        Me.btnDELETE.UseVisualStyleBackColor = True
+        '
+        'btnCANCEL
+        '
+        Me.btnCANCEL.Location = New System.Drawing.Point(286, 140)
+        Me.btnCANCEL.Name = "btnCANCEL"
+        Me.btnCANCEL.Size = New System.Drawing.Size(75, 23)
+        Me.btnCANCEL.TabIndex = 11
+        Me.btnCANCEL.Text = "&CANCEL"
+        Me.btnCANCEL.UseVisualStyleBackColor = True
+        '
+        'txtNAMABARANG
+        '
+        Me.txtNAMABARANG.Location = New System.Drawing.Point(141, 33)
+        Me.txtNAMABARANG.Name = "txtNAMABARANG"
+        Me.txtNAMABARANG.Size = New System.Drawing.Size(220, 20)
+        Me.txtNAMABARANG.TabIndex = 19
+        '
+        'txtHARGA
+        '
+        Me.txtHARGA.Location = New System.Drawing.Point(141, 69)
+        Me.txtHARGA.Name = "txtHARGA"
+        Me.txtHARGA.Size = New System.Drawing.Size(220, 20)
+        Me.txtHARGA.TabIndex = 20
+        '
+        'txtMEREKBARANG
+        '
+        Me.txtMEREKBARANG.Location = New System.Drawing.Point(144, 104)
+        Me.txtMEREKBARANG.Name = "txtMEREKBARANG"
+        Me.txtMEREKBARANG.Size = New System.Drawing.Size(220, 20)
+        Me.txtMEREKBARANG.TabIndex = 21
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(435, 283)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(396, 182)
+        Me.Controls.Add(Me.txtMEREKBARANG)
+        Me.Controls.Add(Me.txtHARGA)
+        Me.Controls.Add(Me.txtNAMABARANG)
+        Me.Controls.Add(Me.btnCANCEL)
+        Me.Controls.Add(Me.btnDELETE)
+        Me.Controls.Add(Me.btnUPDATE)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnINSERT)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "InventoryStock"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnINSERT As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnUPDATE As Button
+    Friend WithEvents btnDELETE As Button
+    Friend WithEvents btnCANCEL As Button
+    Friend WithEvents txtNAMABARANG As TextBox
+    Friend WithEvents txtHARGA As TextBox
+    Friend WithEvents txtMEREKBARANG As TextBox
 End Class
--- UI Database Stocks/MDIParent1.vb
+++ UI Database Stocks/MDIParent1.vb
@@ -6,33 +6,35 @@ Public Class MDIParent1
         ' Create a new instance of the child form.
         Dim ChildForm As New System.Windows.Forms.Form
         ' Make it a child of this MDI form before showing it.
-        ChildForm.MdiParent = Me
+        'ChildForm.MdiParent = Me
 
-        m_ChildFormNumber += 1
-        ChildForm.Text = "Window " & m_ChildFormNumber
+        'm_ChildFormNumber += 1
+        'ChildForm.Text = "Window " & m_ChildFormNumber
 
         ChildForm.Show()
     End Sub
 
     Private Sub OpenFile(ByVal sender As Object, ByVal e As EventArgs) Handles OpenToolStripMenuItem.Click, OpenToolStripButton.Click
-        Dim OpenFileDialog As New OpenFileDialog
-        OpenFileDialog.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
-        OpenFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
-        If (OpenFileDialog.ShowDialog(Me) = System.Windows.Forms.DialogResult.OK) Then
-            Dim FileName As String = OpenFileDialog.FileName
-            ' TODO: Add code here to open the file.
-        End If
+        'Dim OpenFileDialog As New OpenFileDialog
+        'OpenFileDialog.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        'OpenFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+        'If (OpenFileDialog.ShowDialog(Me) = System.Windows.Forms.DialogResult.OK) Then
+        '    Dim FileName As String = OpenFileDialog.FileName
+        '    ' TODO: Add code here to open the file.
+        'End If
+        Form1.Show()
     End Sub
 
     Private Sub SaveAsToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles SaveAsToolStripMenuItem.Click
-        Dim SaveFileDialog As New SaveFileDialog
-        SaveFileDialog.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
-        SaveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+        'Dim SaveFileDialog As New SaveFileDialog
+        'SaveFileDialog.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        'SaveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
 
-        If (SaveFileDialog.ShowDialog(Me) = System.Windows.Forms.DialogResult.OK) Then
-            Dim FileName As String = SaveFileDialog.FileName
-            ' TODO: Add code here to save the current contents of the form to a file.
-        End If
+        'If (SaveFileDialog.ShowDialog(Me) = System.Windows.Forms.DialogResult.OK) Then
+        '    Dim FileName As String = SaveFileDialog.FileName
+        '    ' TODO: Add code here to save the current contents of the form to a file.
+        'End If
+        Form1.Show()
     End Sub
 
 
